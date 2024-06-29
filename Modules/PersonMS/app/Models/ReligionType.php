3fe7d653ea2766a4b5d667b006065400453fb3d5
alighanati <skankhunt125@gmail.com>
2024-06-29 15:41:52 +0330

    continue working on kargozini

---
+++ Modules/PersonMS/app/Models/ReligionType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\PersonMS\app\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\PersonMS\Database\factories\ReligionTypeFactory;
+
+class ReligionType extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+
+    //protected $fillable = [];
+
+    public $timestamps = false;
+
+}
