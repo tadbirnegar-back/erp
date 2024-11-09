ce88374cbc748039162eaf775caa205e409b79cb
Yasin-Ahanj <yassin.ahanj@gmail.com>
2024-11-09 08:46:58 +0330

    Fet:Add Emp Notification

--- Modules/HRMS/app/Http/Controllers/EmployeeController.php
+++ Modules/HRMS/app/Http/Controllers/EmployeeController.php
@@ -30,10 +30,12 @@ use Modules\HRMS\app\Models\IsarStatus;
 use Modules\HRMS\app\Models\Job;
 use Modules\HRMS\app\Models\LevelOfEducation;
 use Modules\HRMS\app\Models\Position;
+use Modules\HRMS\app\Models\RecruitmentScript;
 use Modules\HRMS\app\Models\Relative;
 use Modules\HRMS\app\Models\RelativeType;
 use Modules\HRMS\app\Models\Resume;
 use Modules\HRMS\app\Models\ScriptType;
+use Modules\HRMS\app\Notifications\AddEmployeeNotification;
 use Modules\PersonMS\app\Http\Traits\PersonTrait;
 use Modules\PersonMS\app\Models\Person;
 use Modules\PersonMS\app\Models\Religion;
@@ -89,7 +91,7 @@ class EmployeeController extends Controller
             DB::beginTransaction();
 
             // $data['userID'] = \Auth::user()->id;
-            $data['userID'] = User::find(2129)->id;
+            $data['userID'] = User::find(2126)->id;
             if ($request->isNewAddress) {
                 $address = $this->addressStore($data);
 
@@ -101,16 +103,17 @@ class EmployeeController extends Controller
                 $this->naturalUpdate($data, $data['personID']) :
                 $this->naturalStore($data);
 
+
             $data['personID'] = $personResult->person->id;
             $data['password'] = $data['nationalCode'];
 
             $user = $this->isPersonUserCheck($personResult->person);
             $user = $user ?? $this->storeUser($data);
 
-            return response()->json($user);
-
             $personAsEmployee = $this->isEmployee($data['personID']);
+
             $employee = !is_null($personAsEmployee) ? $this->employeeUpdate($data, $personAsEmployee) : $this->employeeStore($data);
+
 
             $workForce = $employee->workForce;
             //additional info insertion
@@ -151,10 +154,16 @@ class EmployeeController extends Controller
             }
             DB::commit();
 
-            $user->notify(new ScriptExpireNotification($person->display_name, $ExpDateFarsi, $scriptTypeName, $ounit->name));
+
+            $userSMSsend = User::find($data['userID']);
+            $username = $personResult->person->display_name;
+            $position = RecruitmentScript::with('position', 'organizationUnit')->where('employee_id', 2124)->first();
+            $positionName = $position->position->name;
+            $orginazationName = $position->organizationUnit->name;
 
 
-            return response()->json($employee);
+            $userSMSsend->notify(new AddEmployeeNotification($username, $positionName, $orginazationName));
+
 
         } catch (Exception $e) {
             DB::rollBack();
