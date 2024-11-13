b512672d64fa0014ddb67c037d28c934f52ff2df
alighanati <skankhunt125@gmail.com>
2024-11-13 20:29:41 +0330

    bugfix: fix status seeder className

---
+++ Modules/HRMS/database/migrations/2024_11_13_194105_add_operator_id_to_recruitment_script_status_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('recruitment_script_status', function (Blueprint $table) {
+            $table->unsignedBigInteger('operator_id')->nullable();
+
+            $table->foreign('operator_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('recruitment_script_status', function (Blueprint $table) {
+
+        });
+    }
+};
