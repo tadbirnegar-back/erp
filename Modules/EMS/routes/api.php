1161b74b758ce1e85fde6dc011e13d9a0e816ab6
Yasin-Ahanj <yassin.ahanj@gmail.com>
2024-11-19 10:31:11 +0330

    Done

--- Modules/EMS/routes/api.php
+++ Modules/EMS/routes/api.php
@@ -67,6 +67,7 @@ Route::middleware(['auth:api', 'route'])->prefix('v1')->group(function () {
 
     Route::put('mes/meeting/ChangeMeetingDate/{id}', [\Modules\EMS\app\Http\Controllers\MeetingController::class, 'changeMeetingDate']);
 
+    Route::post('mes/ounitNames/liveSearch', [\Modules\EMS\app\Http\Controllers\EMSController::class, 'liveSearch']);
 });
 
 Route::middleware(['auth:api'])->prefix('v1')->group(function () {
@@ -84,4 +85,3 @@ Route::middleware([])->prefix('v1')->group(function () {
     Route::post('mes/board/register', [EMSController::class, 'registerHetaatMember']);
 
 });
-Route::post('mes/ounitNames/liveSearch', [\Modules\EMS\app\Http\Controllers\EMSController::class, 'liveSearch']);
