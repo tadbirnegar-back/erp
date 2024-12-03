da2431290e2506f4aab6c3718ab2907ee1cf6e12
Yasin-Ahanj <yassin.ahanj@gmail.com>
2024-12-03 08:51:11 +0330

    Fixed Code about selection of Meetings

---
+++ Modules/EMS/tests/Unit/MeetingTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\EMS\tests\Unit;
+
+use Tests\TestCase;
+
+class MeetingTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+}
