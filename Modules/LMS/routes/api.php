227b60134f2d517bb52315e5220d0bfde75c44e0
AmirHossein Alinezhad <amiralnzd@gmail.com>
2024-12-07 13:24:08 +0330

    fixed Routes

--- Modules/LMS/routes/api.php
+++ Modules/LMS/routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 //    Route::get('lms', fn (Request $request) => $request->user())->name('lms');
 //});
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
+Route::middleware(['auth:api', 'route'])->prefix('v1')->name('api.')->group(function () {
     Route::post('/teacher/add', [\Modules\LMS\app\Http\Controllers\TeacherController::class, 'store']);
 
 });
