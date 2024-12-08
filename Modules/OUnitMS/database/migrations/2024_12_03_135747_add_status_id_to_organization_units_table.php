eb993e37fa0a6df2531cc65ac8de0fb12c206489
alighanati <skankhunt125@gmail.com>
2024-12-08 09:44:45 +0330

    bigfix: bigfix

--- Modules/OUnitMS/database/migrations/2024_12_03_135747_add_status_id_to_organization_units_table.php
+++ Modules/OUnitMS/database/migrations/2024_12_03_135747_add_status_id_to_organization_units_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,7 @@ return new class extends Migration
     {
         Schema::table('organization_units', function (Blueprint $table) {
             $table->unsignedBigInteger('status_id')->nullable();
-            $table->foreign('status_id')->references('id')->on('statuses')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('status_id')->references('id')->on('statuses')->onUpdate('set null')->onDelete('set null');
         });
     }
 
