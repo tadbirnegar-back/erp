9f91b173b723a9a6bcbf4ad60bf142f1708618fc
alighanati <skankhunt125@gmail.com>
2024-12-12 10:31:45 +0330

    hotfix: fix base data for enactment-list

--- Modules/EMS/app/Http/Controllers/EnactmentController.php
+++ Modules/EMS/app/Http/Controllers/EnactmentController.php
@@ -95,10 +95,10 @@ class EnactmentController extends Controller
             $data = $request->all();
             $enactments = $this->indexPendingForArchiveStatusEnactment($data, $ounits, $user->id);
 
-            $districtIDs = $user->load('activeDistrictRecruitmentScript');
+//            $districtIDs = $user->load('activeDistrictRecruitmentScript');
             $statuses = Enactment::GetAllStatuses();
             $enactmentReviews = EnactmentReview::GetAllStatuses();
-            return response()->json(['data' => $enactments, 'statusList' => $statuses, 'enactmentReviews' => $enactmentReviews, 'ounits' => $user->activeDistrictRecruitmentScript->pluck('organizationUnit'), 'districtIDs' => $districtIDs]);
+            return response()->json(['data' => $enactments, 'statusList' => $statuses, 'enactmentReviews' => $enactmentReviews, 'ounits' => $user->activeDistrictRecruitmentScript->pluck('organizationUnit'), 'districtIDs' => $ounits]);
         } catch (Exception $e) {
             return response()->json(['message' => 'خطا در دریافت اطلاعات'], 500);
         }
