f63623f758f6cb5edeedb5095149bdac339a68a6
Yasin-Ahanj <yassin.ahanj@gmail.com>
2024-12-15 12:01:11 +0330

    changed some names

--- Modules/LMS/database/seeders/LMSDatabaseSeeder.php
+++ Modules/LMS/database/seeders/LMSDatabaseSeeder.php
@@ -20,6 +20,7 @@ class LMSDatabaseSeeder extends Seeder
 //            AnswerSheetStatusSeeder::class,
 //            ProcessStatusSeeder::class,
             CourseStatusSeeder::class,
+            LessonStatusSeeder::class,
         ]);
     }
 }
