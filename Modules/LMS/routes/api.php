9d48bed8b50633ed3e19547847a24dc6862a0c86
AmirHossein Alinezhad <amiralnzd@gmail.com>
2024-12-15 14:05:51 +0330

    fixed route lms/courses/lesson/list

--- Modules/LMS/routes/api.php
+++ Modules/LMS/routes/api.php
@@ -34,6 +34,7 @@ Route::middleware([])->prefix('v1')->name('api.')->group(function () {
 Route::middleware(['auth:api', 'route'])->prefix('v1')->group(function () {
     Route::post('/lms/teachers/add', [TeacherController::class, 'store']);
     Route::post('/lms/courses/list', [\Modules\LMS\app\Http\Controllers\CourseController::class, 'courseList']);
+    Route::post('/lms/courses/lesson/list', [CourseController::class, 'lesson_index']);
 
 
 });
@@ -42,4 +43,3 @@ Route::middleware(['auth:api'])->prefix('v1')->group(function () {
     Route::get('/lms/courses/{id}', [CourseController::class, 'show']);
 });
 
-Route::post('/lms/courses/lesson/list', [CourseController::class, 'lesson_index']);
