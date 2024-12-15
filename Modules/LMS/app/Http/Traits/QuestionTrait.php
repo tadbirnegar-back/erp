c41dc3ef83042c504e9270ea9bbb288397f7d9f4
AmirHossein Alinezhad <amiralnzd@gmail.com>
2024-12-15 12:36:19 +0330

    donnt notice

--- Modules/LMS/app/Http/Traits/QuestionTrait.php
+++ Modules/LMS/app/Http/Traits/QuestionTrait.php
@@ -12,14 +12,14 @@ trait QuestionTrait
 
         $status = $this->questionActiveStatus();
         $question = new Question();
-        $question->title = $data['title'];
-        $question->creator_id = $data['creator_id'];
-        $question->difficulty_id = $data['difficulty_id'] ?? null;
-        $question->lesson_id = $data['lesson_id'] ?? null;
-        $question->question_type_id = $data['question_type_id'] ?? null;
-        $question->repository_id = $data['repository_id'] ?? null;
+        $question->title = $data['title'] ?? null;
+        $question->creator_id = $data['creatorId'] ?? null;
+        $question->difficulty_id = $data['difficultyId'] ?? null;
+        $question->lesson_id = $data['lessonId'] ?? null;
+        $question->question_type_id = $data['questionTypeId'] ?? null;
+        $question->repository_id = $data['repositoryId'] ?? null;
         $question->status_id = $status->id;
-        $question->create_date = $data['create_date'] ?? null;
+        $question->create_date = $data['createDate'] ?? null;
         $question->save();
         return $question->load('lesson', 'creator', 'difficulty', 'questionType', 'status', 'repository');
 
@@ -27,7 +27,7 @@ trait QuestionTrait
 
     public function questionActiveStatus()
     {
-        return Question::status()->firstWhere('name', QuestionsEnum::ACTIVE->value);
+        return Question::status()->firstWhere('name', '=', QuestionsEnum::ACTIVE->value);
     }
 
     public function questionInActiveStatus()
