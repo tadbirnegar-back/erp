d1accf4c239e3eb76c48949c13cfae546bc55af4
Yasin-Ahanj <yassin.ahanj@gmail.com>
2024-12-21 09:38:45 +0330

    removed trace

--- Modules/LMS/app/Http/Controllers/CourseController.php
+++ Modules/LMS/app/Http/Controllers/CourseController.php
@@ -37,8 +37,7 @@ class CourseController extends Controller
             return response()->json($componentsToRenderWithData);
         } catch (\Exception $e) {
             DB::rollBack();
-            return response()->json(['error' => $e->getMessage(),
-                'line' => $e->getLine()], 500);
+            return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
