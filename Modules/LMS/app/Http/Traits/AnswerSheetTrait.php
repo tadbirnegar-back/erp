84ba504651bafeec7ff1cb83011885335c3ed3c1
AmirHossein Alinezhad <amiralnzd@gmail.com>
2025-01-10 11:54:03 +0330

    changed branch

--- Modules/LMS/app/Http/Traits/AnswerSheetTrait.php
+++ Modules/LMS/app/Http/Traits/AnswerSheetTrait.php
@@ -3,10 +3,7 @@
 namespace Modules\LMS\app\Http\Traits;
 
 use Modules\LMS\app\Http\Enums\AnswerSheetStatusEnum;
-use Modules\LMS\app\Models\Answers;
 use Modules\LMS\app\Models\AnswerSheet;
-use Modules\LMS\app\Models\Option;
-use Modules\LMS\app\Models\QuestionExam;
 
 trait AnswerSheetTrait
 {
@@ -25,64 +22,15 @@ trait AnswerSheetTrait
         return AnswerSheet::GetAllStatuses()->firstWhere('name', AnswerSheetStatusEnum::DECLINED->value);
     }
 
-
-    public function StoringAnswerSheet($examId, $student, $optionIDs,)
+    public function storingAnswerSheet()
     {
-        $score = $this->score($examId, $optionIDs);
-        $status = $this->ScoreStatus($score);
 
-        $finish = now();
-        $start = now();
-        $answerSheet = AnswerSheet::create([
-            'score' => $score,
-            'student_id' => $student->id,
-            'exam_id' => $examId,
-            'finish_date_time' => $finish,
-            'start_date_time' => $start,
-            'status_id' => $status->id,
-        ]);
-
-        foreach ($optionIDs as $questionID => $optionID) {
-            $option = Option::findOrFail($optionID);
-
-            Answers::create([
-                'answer_sheet_id' => $answerSheet->id,
-                'question_id' => $questionID,
-                'value' => $option->title,
-            ]);
-        }
-
-        return $answerSheet;
     }
 
-    public function correctAnswers($optionIDs)
+    public function dataPreparation()
     {
-        $correct = Option::whereIn('id', $optionIDs)
-            ->where('is_correct', 1)
-            ->count();
 
-        return $correct;
     }
 
-    public function score($examId, $optionIDs)
-    {
-        $totalQuestions = QuestionExam::where('exam_id', $examId)->count();
-        $correctAnswers = $this->correctAnswers($optionIDs);
-
-        if ($totalQuestions == 0) {
-            return 0;
-        }
-
-        return ($correctAnswers / $totalQuestions) * 100;
-    }
-
-    public function ScoreStatus($score)
-    {
-        if ($score >= 50) {
-            return $this->answerSheetApprovedStatus();
-        } else {
-            return $this->answerSheetDeclinedStatus();
-        }
-    }
 
 }
