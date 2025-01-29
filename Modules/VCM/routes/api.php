97f90b439ae105a590c181135977caeb2cb355cb
Yasin-Ahanj <yassin.ahanj@gmail.com>
2025-01-29 13:40:28 +0330

    Done

--- Modules/VCM/routes/api.php
+++ Modules/VCM/routes/api.php
@@ -18,12 +18,10 @@ use Modules\VCM\app\Http\Controllers\VersionManagementController;
 Route::middleware(['auth:api', 'route'])->prefix('v1')->group(function () {
     Route::post('/vcm/version/store' , [VersionManagementController::class , 'storeVersion']);
     Route::post('/vcm/version/list' , [VersionManagementController::class , 'indexVersion']);
-    Route::get('/vcm/version/show' , [VersionManagementController::class , 'showVersion']);
+    Route::get('/vcm/modules/list' , [VersionManagementController::class , 'indexModules']);
 });
 
 Route::middleware(['auth:api'])->prefix('v1')->group(function () {
-    Route::get('/vcm/modules/list' , [VersionManagementController::class , 'indexModules']);
-    Route::post('/vcm/version/list' , [VersionManagementController::class , 'indexVersion']);
-    Route::post('/vcm/version/store' , [VersionManagementController::class , 'storeVersion']);
+    Route::get('/vcm/version/show' , [VersionManagementController::class , 'showVersion']);
 });
 
