0a08fd7260e1453544f9316df2833ce444c49ae2
AmirHossein Alinezhad <amiralnzd@gmail.com>
2025-02-14 08:50:44 +0330

    fixed resource and set two data as hardcode

--- Modules/LMS/app/Resources/CourseReportResource.php
+++ Modules/LMS/app/Resources/CourseReportResource.php
@@ -14,9 +14,46 @@ class CourseReportResource extends JsonResource
         return [
             'course' => [
                 'courseTitle' => $this['course']['courseTitle'] ?? null,
-                'chapters_count' => $this['course']['chaptersCount'] ?? null,
-                'all_active_lessons_count' => $this['course']['allActiveLessonsCount'] ?? null,
+                'chapters_count' => $this['course']['chapters_count'] ?? null,
+                'all_active_lessons_count' => $this['course']['all_active_lessons_count'] ?? null,
             ],
+            'StudentsCount' => $this['allStudents'] ?? null,
+            'certificatesCount' => $this['certificatesCount'] ?? null,
+            'scoreAverageAndEnrolledStudents' => [
+                'average' => $this['scoreAverageAndEnrolledStudents']['average'] ?? null,
+                'EnrolledStudents' => $this['scoreAverageAndEnrolledStudents']['EnrolledStudents'] ?? null,
+            ],
+            'durationOfAudio' => [
+                'duration' => $this['durationOfAudio']['duration'] ?? null,
+                'consume_round' => $this['durationOfAudio']['consume_round'] ?? null,
+                'total' => $this['durationOfAudio']['total'] ?? null,
+                'averageOfAudio' => $this['durationOfAudio']['averageOfAudio'] ?? null,
+            ],
+            'durationOfVideo' => [
+                'duration' => $this['durationOfVideo']['duration'] ?? null,
+                'consume_round' => $this['durationOfVideo']['consume_round'] ?? null,
+                'total' => $this['durationOfVideo']['total'] ?? null,
+                'averageOfVideo' => $this['durationOfVideo']['averageOfVideo'] ?? null,
+            ],
+            'totalPlayedDuration' => $this['totalPlayedDuration'] ?? null,
+            'subInfoOfAllEnrollmentStudents' => [
+                'approvedStudents' => $this['approvedStudents'] ?? null,
+                'declinedStudents' => $this['declinedStudents'] ?? null,
+            ],
+            'subInfoOfAllStudents' => [
+                'includedStudents' => 100,
+                'hasNotParticipated' => 50,
+                'studyCompleted' => $this['subCount']['studyCompleted'] ?? null,
+                'isStudying' => $this['subCount']['isStudying'] ?? null,
+            ],
+            'scoreAndMonthChart' => array_map(function ($item) {
+                return [
+                    'month' => $item['month'] ?? null,
+                    'average_score' => $item['average_score'] ?? null,
+                ];
+            }, $this['scoreAndMonthChart'] ?? []),
+
+
         ];
     }
 
