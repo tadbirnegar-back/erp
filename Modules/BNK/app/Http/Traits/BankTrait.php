b4460004eef5bbd6a873db7fa455f1db9a6a2094
Yasin-Ahanj <yassin.ahanj@gmail.com>
2025-04-14 18:48:03 +0330

    saved

--- Modules/BNK/app/Http/Traits/BankTrait.php
+++ Modules/BNK/app/Http/Traits/BankTrait.php
@@ -2,6 +2,7 @@
 
 namespace Modules\BNK\app\Http\Traits;
 
+use Illuminate\Support\Facades\Cache;
 use Modules\BNK\app\Http\Enums\BankAccountStatusEnum;
 use Modules\BNK\app\Models\Bank;
 use Modules\BNK\app\Models\BankAccount;
