52da8d54cf3dd26b69292870db60dea285ff83f9
Yasin-Ahanj <yassin.ahanj@gmail.com>
2025-04-21 08:28:14 +0330

    saved

--- Modules/AAA/routes/api.php
+++ Modules/AAA/routes/api.php
@@ -58,11 +58,11 @@ Route::prefix('v1')->group(function () {
 
 Route::prefix('v2')->group(function () {
     Route::post('/login', [LoginControllerV2::class, 'getToken']);
-    Route::post('/refreshToken', [LoginControllerV2::class, 'refreshToken']);
-    Route::get('/getPermissions', [LoginControllerV2::class, 'getPermission']);
-    Route::get('/getUserInfo', [LoginControllerV2::class, 'getUserInfo']);
-    Route::get('/checkPayed', [LoginControllerV2::class, 'checkPayed']);
-    Route::post('/loginWithOtp', [LoginControllerV2::class, 'loginWithOtp']);
+    Route::post('/refresh-token', [LoginControllerV2::class, 'refreshToken']);
+    Route::get('/get-permissions', [LoginControllerV2::class, 'getPermission']);
+    Route::get('/get-user-info', [LoginControllerV2::class, 'getUserInfo']);
+    Route::get('/check-payed', [LoginControllerV2::class, 'checkPayed']);
+    Route::post('/login-with-otp', [LoginControllerV2::class, 'loginWithOtp']);
 
 });
 
